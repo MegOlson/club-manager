c46589fa780bb7293b066caf7e4353f85f0d5d4e
mo <meganannetteolson@yahoo.com>
2017-11-03 08:30:39 -0700

    Add sample members and import json to database.

---
+++ src/app/member.model.ts
@@ -0,0 +1,10 @@
+export class Member {
+  constructor(
+    public name: string,
+    public age: number,
+    public email: string,
+    public specialty: string,
+    public experience: string,
+    public favoriteCrop: string
+  ) {}
+}
